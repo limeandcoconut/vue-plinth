f0a7135237a40a6848aaf1579756c1d3b0111237
Jacob Smith <js@jacobsmith.tech>
2020-03-13 16:43:23 -0700

    Fix echo

--- eject.sh
+++ eject.sh
@@ -2,4 +2,4 @@
 echo "Running rm -rf .git"
 rm -rf .git
 
-echo "\nChange: config/config.js \nAdd icons \nAuthor, colors, twitter names in config/meta.config.js \n"
+echo -e "\nChange: config/config.js \nAdd icons \nAuthor, colors, twitter names in config/meta.config.js \n"
